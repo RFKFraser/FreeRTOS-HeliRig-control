b944d99fb293afc3391571b671f6ad68ad20b619
Ronan Fraser <57420202+RFKFraser@users.noreply.github.com>
2020-02-18 13:14:17 +1300

    Update out_task.c

--- emulator/tasks/out_task.c
+++ emulator/tasks/out_task.c
@@ -9,7 +9,6 @@
 // Helicopter Rig Emulator
 //
 // Module: out_task.c
-// Author: Matthew Northcott / mno32@uclive.ac.nz
 // 
 //*****************************************************************************
 
